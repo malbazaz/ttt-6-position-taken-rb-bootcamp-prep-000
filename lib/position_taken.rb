573c32f05de55bc8cda090109b91461bf420f6fa
Mo Albazaz <malbazaz@mba2018.hbs.edu>
2018-06-09 17:02:32 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
 
   if board[index] == "" || board[index] == " " || board[index] == nil 
     false  
-  elsif board[index] == "X" || "O" || "x" || "o"
+  elsif board[index] == "X" || board[index] == "O"
     true 
   end 
 end 
